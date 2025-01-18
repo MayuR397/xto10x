af9ffac55023b6eafb8a01c2ce555a09a988c0e4
Mayur Khachane <mayurkhachane67@gmail.com>
2025-01-18 19:55:32 +0530

    Update ProfessionalImageComponent.jsx

--- src/firstDeploy/ProfessionalImageComponent.jsx
+++ src/firstDeploy/ProfessionalImageComponent.jsx
@@ -30,7 +30,7 @@ const ProfessionalImageComponent = () => {
 
         {/* YouTube Button */}
         <a
-          href="https://www.youtube.com/watch?v=kb4joSzUu4k"
+          href="https://www.youtube.com/watch?v=NkPnkVN_Lrg"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center justify-center gap-2 bg-red-500 text-white py-3 px-6 rounded-lg shadow hover:bg-red-600 transition duration-300"
