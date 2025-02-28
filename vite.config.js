a83e6a4b38742beccdbf17ebd04b39920da2087c
Mayur Khachane <mayurkhachane@Mayurs-MacBook-Pro.local>
2025-02-28 19:16:52 +0530

    updated app.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  base: '/', // Important for correct asset linking
+});
